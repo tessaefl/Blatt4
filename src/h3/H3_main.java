0c01ac5b807f9489af3a668a300d31a5ff1748f7
tessaefl <tessa.loy46@gmail.com>
2025-11-12 15:56:37 +0100

    h3 update

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -5,14 +5,16 @@ public class H3_main {
 
         float celsiusFloat = 14.2F;
         float fahrenheitFloat = 2.0F;
-        double celsiusDouble = 3.0;
+        double celsiusDouble = 14.2;
         double fahrenheitdouble = 4.0;
 
-        celsiusDouble = (fahrenheitFloat - 32) * 5 / 9;
-        fahrenheitdouble = celsiusFloat * 9 / 5 + 32;
+        fahrenheitdouble = (celsiusDouble * 9 / 5) + 32;
+        fahrenheitFloat = (celsiusFloat * 9 / 5) + 32;
 
         System.out.println("Celsius (float): " + celsiusFloat);
         System.out.println("Fahrenheit (double): " + fahrenheitdouble);
+        System.out.println("Celsius (double): " + celsiusDouble);
+        System.out.println("Fahrenheit (float): " + fahrenheitFloat);
 
     }
 
